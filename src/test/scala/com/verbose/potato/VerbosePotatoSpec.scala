0156a7a5be5fc9f0f8ba8723d8922787307dd06f
sallareznov <salla@colisweb.com>
2021-09-06 13:13:40 +0000

    feat: write test for the function that calculates the number of Sundays between two dates

--- src/test/scala/com/verbose/potato/VerbosePotatoSpec.scala
+++ src/test/scala/com/verbose/potato/VerbosePotatoSpec.scala
@@ -8,25 +8,33 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.TableDrivenPropertyChecks._
 import org.scalatest.prop.TableFor2
 
+import java.time.LocalDate
+
 final class VerbosePotatoSpec extends AnyFunSuiteLike with Matchers {
 
-  val table: TableFor2[PosInt, String] =
-    Table(
-      ("number", "suffix"),
-      (1, "1st"),
-      (22, "22nd"),
-      (33, "33rd"),
-      (44, "44th"),
-      (55, "55th"),
-      (66, "66th"),
-      (77, "77th"),
-      (88, "88th"),
-      (99, "99th"),
-      (110, "110th")
-    )
+  test("ordinal indicator suffix for a number") {
+    val table: TableFor2[PosInt, String] =
+      Table(
+        ("number", "suffix"),
+        (1, "1st"),
+        (22, "22nd"),
+        (33, "33rd"),
+        (44, "44th"),
+        (55, "55th"),
+        (66, "66th"),
+        (77, "77th"),
+        (88, "88th"),
+        (99, "99th"),
+        (110, "110th")
+      )
 
-  test("ordinal indicator suffix") {
-    forAll(table)((number, suffix) => ordinalIndicatorSuffix(number) shouldBe suffix)
+      forAll(table)((number, suffix) => ordinalIndicatorSuffix(number) shouldBe suffix)
   }
+
+  test("number of sundays between two dates") {
+    numberOfSundaysBetween(date("01-05-2021"), date("30-05-2021")) shouldBe 5
+  }
+
+  private def date(d: String): LocalDate = LocalDate.parse(d)
 
 }
