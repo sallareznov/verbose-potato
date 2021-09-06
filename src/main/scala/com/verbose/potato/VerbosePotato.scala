67634e1fb59ee519bd492f89e5ce98ac029eb32f
sallareznov <salla@colisweb.com>
2021-09-06 15:58:38 +0000

    feat: implement a function that calculates the number of sundays between two dates

--- src/main/scala/com/verbose/potato/VerbosePotato.scala
+++ src/main/scala/com/verbose/potato/VerbosePotato.scala
@@ -2,6 +2,13 @@ package com.verbose.potato
 
 import eu.timepit.refined.types.numeric.PosInt
 
+import java.time.DayOfWeek._
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import scala.math.Ordering.Implicits.infixOrderingOps
+import scala.util.Try
+
 object VerbosePotato {
 
   def ordinalIndicatorSuffix(n: PosInt): String =
@@ -11,5 +18,16 @@ object VerbosePotato {
       case 3 => s"${n}rd"
       case _ => s"${n}th"
     }
+
+  def numberOfSundaysBetween(dateFrom: String, dateTo: String): Either[String, Long] =
+    (parseDate(dateFrom), parseDate(dateTo)) match {
+      case (Some(d1), Some(d2)) if d1 > d2 => Left(s"dateFrom is after dateTo ($dateFrom < $dateTo)")
+      case (Some(d1), Some(d2))            =>
+        val isThereARemainingSunday = d1.getDayOfWeek == SUNDAY || d2.getDayOfWeek == SUNDAY || d2.getDayOfWeek < d1.getDayOfWeek
+        if (isThereARemainingSunday) Right(1 + ChronoUnit.WEEKS.between(d1, d2)) else Right(ChronoUnit.WEEKS.between(d1, d2))
+      case _                               => Left("invalid format for one or both dates")
+    }
+
+  def parseDate(d: String): Option[LocalDate] = Try(LocalDate.parse(d, DateTimeFormatter.ofPattern("dd-MM-yyyy"))).toOption
 
 }
--- src/test/scala/com/verbose/potato/VerbosePotatoSpec.scala
+++ src/test/scala/com/verbose/potato/VerbosePotatoSpec.scala
@@ -8,8 +8,6 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.TableDrivenPropertyChecks._
 import org.scalatest.prop.TableFor2
 
-import java.time.LocalDate
-
 final class VerbosePotatoSpec extends AnyFunSuiteLike with Matchers {
 
   test("ordinal indicator suffix for a number") {
@@ -32,9 +30,14 @@ final class VerbosePotatoSpec extends AnyFunSuiteLike with Matchers {
   }
 
   test("number of sundays between two dates") {
-    numberOfSundaysBetween(date("01-05-2021"), date("30-05-2021")) shouldBe 5
-  }
+    assert(numberOfSundaysBetween("01-05-2021", "30-05-2021") == Right(5), "almost whole month")
+    assert(numberOfSundaysBetween("01-05-2021", "05-05-2021") == Right(1), "5 days with sunday in between")
+    assert(numberOfSundaysBetween("03-05-2021", "07-05-2021") == Right(0), "4 days without sunday in between")
+    assert(numberOfSundaysBetween("02-05-2021", "02-05-2021") == Right(1), "same day (sunday)")
+    assert(numberOfSundaysBetween("08-05-2021", "08-05-2021") == Right(0), "same day (not sunday)")
 
-  private def date(d: String): LocalDate = LocalDate.parse(d)
+    assert(numberOfSundaysBetween("30-05-2021", "01-05-2021").isLeft, "start before end")
+    assert(numberOfSundaysBetween("30-05-2021", "2021-05-01").isLeft, "invalid date format")
+  }
 
 }
